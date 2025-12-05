4e164ffddd92ad2189332c0017dc4d63be9b1f1d
Asier Carrasco González <axiercarrasco@gmail.com>
2025-12-05 23:20:40 +0100

    fix: rearrangement of routes for coherence with frontend options

--- routes/api.php
+++ routes/api.php
@@ -5,25 +5,39 @@ use App\Http\Controllers\Api\V1\BookController;
 use App\Http\Controllers\Api\V1\PatronController;
 use App\Http\Controllers\Api\V1\LoanController;
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\DashboardController;
 
 Route::prefix('v1')->group(function () {
-
-
     Route::post('/auth/login', [AuthController::class, 'login']);
 
     Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{id}', [BookController::class, 'show']);
 
     Route::middleware('auth:api')->group(function () {
         Route::get('/auth/me', [AuthController::class, 'me']);
+        Route::get('/dashboard', [DashboardController::class, 'index']);
 
-        Route::post('/books', [BookController::class, 'store']);
-        Route::get('/books/{id}', [BookController::class, 'show']);
-        Route::put('/books/{book}', [BookController::class, 'update']);
-        Route::delete('/books/{book}', [BookController::class, 'destroy']);
+        Route::get('/patrons/me', [PatronController::class, 'showMe']);
+        Route::put('/patrons/me', [PatronController::class, 'updateMe']);
 
-        Route::apiResource('patrons', PatronController::class);
+        Route::get('/loans/my', [LoanController::class, 'myLoans']);
+        Route::put('/loans/my/{loan}', [LoanController::class, 'requestExtension']);
 
-        Route::get('/loans', [LoanController::class, 'index']);
-        Route::post('/loans', [LoanController::class, 'store']);
+        Route::middleware('admin')->group(function () {
+            Route::post('/books', [BookController::class, 'store']);
+            Route::put('/books/{book}', [BookController::class, 'update']);
+            Route::delete('/books/{book}', [BookController::class, 'destroy']);
+
+            Route::get('/patrons', [PatronController::class, 'index']);
+            Route::get('/patrons/{patron}', [PatronController::class, 'show']);
+            Route::post('/patrons', [PatronController::class, 'store']);
+            Route::put('/patrons/{patron}', [PatronController::class, 'update']);
+            Route::delete('/patrons/{patron}', [PatronController::class, 'destroy']);
+
+            Route::get('/loans', [LoanController::class, 'index']);
+            Route::post('/loans', [LoanController::class, 'store']);
+            Route::put('/loans/{loan}', [LoanController::class, 'update']);
+            Route::delete('/loans/{loan}', [LoanController::class, 'destroy']);
+        });
     });
 });
